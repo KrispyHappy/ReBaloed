dcf2bee7952bea083a67c83aec9dab42c46e538d
KrispyHappy <94727340+KrispyHappy@users.noreply.github.com>
2024-10-06 03:06:49 -0700

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,11 @@
+{
+	re_tag = true;
+	re_rare = true;
+    re_credit_card = true;
+	re_loyalty_card = true;
+	re_8_ball = true;
+	re_delayed_grat = true;
+	re_nova = true;
+	re_square = true;
+	re_acrobat = true;
+}
